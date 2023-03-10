ef3b79e682f15ad773f02794fb04023342014b12
AndreichukVladlena <andreychuk04@mail.ru>
2023-03-10 19:47:31 +0300

    operation function added

--- lab1/main.py
+++ lab1/main.py
@@ -1,2 +1,4 @@
 from utilities import hello_world
+from utilities import operation
 hello_world()
+print(operation(6, 7, "mult"))
--- lab1/utilities.py
+++ lab1/utilities.py
@@ -1,2 +1,14 @@
 def hello_world():
     print("Hello world!")
+
+def operation(num1, num2, op):
+    if op=="add":
+        return num1+num2
+    elif op=="sub":
+        return num1-num2
+    elif op=="mult":
+        return num1*num2
+    elif op=="div":
+        return num1*num2
+    else:
+        return 0
