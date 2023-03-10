0dd8e9aebfcdb0b4ec3a0db924ef7fa89f6eed1e
AndreichukVladlena <andreychuk04@mail.ru>
2023-03-10 22:00:50 +0300

    save even numbers from list fuction created

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,10 @@
 from utilities import hello_world
 from utilities import operation
+from utilities import save_even_numbers
 hello_world()
+print("6 * 7 = ")
 print(operation(6, 7, "mult"))
+print ("even numbers from list")
+even_num = save_even_numbers()
+for element in even_num:
+    print(element)
--- lab1/utilities.py
+++ lab1/utilities.py
@@ -1,14 +1,24 @@
 def hello_world():
     print("Hello world!")
 
+
 def operation(num1, num2, op):
-    if op=="add":
-        return num1+num2
-    elif op=="sub":
-        return num1-num2
-    elif op=="mult":
-        return num1*num2
-    elif op=="div":
-        return num1*num2
+    if op == "add":
+        return num1 + num2
+    elif op == "sub":
+        return num1 - num2
+    elif op == "mult":
+        return num1 * num2
+    elif op == "div":
+        return num1 * num2
     else:
         return 0
+
+
+def save_even_numbers():
+    numbers = [5, 6, 7, 3, 2, 1, 9, 4555, 2, 2, 21, 23]
+    new_numbers = []
+    for n in numbers:
+        if n % 2 == 0:
+            new_numbers.append(n)
+    return new_numbers
