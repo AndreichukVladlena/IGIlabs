c4bc6c49c591cc4b43130789a05c429036bedd70
vlanuka <andreychuk04@mail.ru>
2023-04-05 00:29:40 +0300

    FileService.py added

--- lab2/FileService.py
+++ lab2/FileService.py
@@ -1,5 +1,9 @@
 import re
 from collections import Counter
+
+abbreviations = ['Mr', 'Mrs', 'Dr', 'etc', 'Prof', 'Gen', 'Sgt', 'Col', 'Capt', 'Lt', 'Rev', 'Hon', 'Pres', 'Gov',
+                 'Sen']
+
 def read_file(file):
     text = ""
     for line in file:
@@ -9,59 +13,49 @@ def read_file(file):
 
 def sentences_counter(text):
     result = [0, 0]
-    reg_ex_non_dec = r"[^a-zA-Z0-9\s]*(?:\.{3}|\.)"
-    # reg_ex_non_dec = r"[^.!?]*(?:\.{3}|\.)"
+    reg_ex_all_sent = r"\s*([^.?!]+)[.?!]"
     reg_ex_dec = r"[^.!?]*(?:[!?])"
-
-    result[0] = len(re.findall(reg_ex_non_dec, text))+len(re.findall(reg_ex_dec, text))
-    result[1] = len(re.findall(reg_ex_non_dec, text))
+    sentences = re.findall(reg_ex_all_sent, text)
+    amount = len(sentences)
+    for item in sentences:
+        for abb in abbreviations:
+            if item == abb:
+                amount -= 1
+    result[0] = amount
+    result[1] = len(re.findall(reg_ex_all_sent, text))
     return result
 
 
 def average_sent(text):
-    reg_ex_sent = r"[^.!?]*[\w']+[^.!?]*[.!?]"
-    print(re.findall(reg_ex_sent, text))
-    amount = 0
     all_length = 0
-    temp_length = 0
-    for symbol in text:
-        if symbol == "." or symbol == "\n" or symbol == "!" or symbol == "?":
-            if temp_length != 0:
-                amount += 1
-                all_length += temp_length
-                temp_length = 0
-        elif symbol == " " or symbol == "\n":
-            continue
-        else:
-            temp_length += 1
-    if amount == 0:
-        amount = 1
-    return int(all_length / amount)
+    amount = 0
+    sent_reg_ex = r"\s*([^.?!]+)[.?!]"
+    sentences = re.findall(sent_reg_ex, text)
+    amount = len(sentences)
+    for item in sentences:
+        for abb in abbreviations:
+            if item == abb:
+                amount -= 1
+    for item in sentences:
+        all_length += len(item)
+    return int(all_length/amount)
 
 
 def average_word(text):
-    amount = 0
     all_length = 0
-    temp_length = 0
-    for symbol in text:
-        if symbol == " " or symbol == "\n" or symbol == "." or symbol == "!" or symbol == "?":
-            if temp_length != 0:
-                amount += 1
-                all_length += temp_length
-                temp_length = 0
-        else:
-            temp_length += 1
-    if amount == 0:
-        amount = 1
-    return int(all_length / amount)
+    reg_ex = r"\W+"
+    words = re.split(reg_ex, text)
+    for item in words:
+        all_length += len(item)
+    return int(all_length / len(words)-1)
+
 
 
 def repeats(text, n, k):
     cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', text.lower())
+    print (cleaned_text)
     words = cleaned_text.split()
     ngrams = [tuple(words[i:i + n]) for i in range(len(words) - n + 1)]
     ngram_counts = Counter(ngrams)
     return ngram_counts.most_common(k)
-
-
 
