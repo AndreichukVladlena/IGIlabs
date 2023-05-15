86e344e84d0b623c73b7456654a6bf58d9d1ce88
vlanuka <andreychuk04@mail.ru>
2023-05-16 00:06:52 +0300

    Tests created

--- for_tests.py
+++ for_tests.py
@@ -1,26 +1,31 @@
 def func(val):
     return val + 5
 
+
 def decorator(func):
     def wrapper(*args, **kwargs):
         res = func(*args, **kwargs)
         return res
-    return wrapper()
+    return wrapper
+
 
 class A:
     field = 5
+
     def test1(self):
-        return self.field-5
+        return self.field - 5
 
     @staticmethod
     def static_test1():
         return A.field
 
+
 class B:
-    message="I'm class B"
+    field = 7
 
     def test2(self):
-        return B.message
+        return self.field + 3
+
 
 class C(A, B):
     pass
